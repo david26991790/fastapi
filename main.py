4bd414152948f00c8a4de4d7cf908c99c7283d8b
david26991790 <p26991790@gmail.com>
2023-06-29 16:06:23 +0800

    202306

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
 from fastapi import FastAPI
 from datetime import datetime
 import sqlite3
-from sqlite3 import Error, Connection
-from typing import Tuple
+from sqlite3 import Error
+from sqlite3 import Connection
 
 app = FastAPI()
-db_file = 'data.db'
 
-
-def create_connection() -> Connection:
+def create_connection(db_file:str) -> Connection | None:
     conn = None
     try:
         conn = sqlite3.connect(db_file)
     except Error as e:
         print(e)
+
     return conn
 
-
-def create_table(conn: Connection):
+def create_table(conn:Connection):
     sql_tasks = """
     CREATE TABLE IF NOT EXISTS iot1(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -26,21 +24,62 @@ def create_table(conn: Connection):
         temperature REAL NOT NULL
     );
     """
+
     try:
         cursor = conn.cursor()
         cursor.execute(sql_tasks)
-    except Error as e:
-        print(e)
+    except:
+        print("error")
 
-
-def insert_project(conn: Connection, project: Tuple[str, float, float]):
+def insert_project(conn:Connection, project:tuple[str,float,float]):
     sql = """
-    INSERT INTO iot1(date, light, temperature)
-    VALUES (?, ?, ?)
+    INSERT INTO iot1(date,light,temperature)
+    VALUES(?,?,?)
     """
     cursor = conn.cursor()
-    cursor.execute(sql, project)
+    cursor.execute(sql,project)
     conn.commit()
 
+def select_all_tasks(conn:Connection,count:int):
+    sql = f"""
+        SELECT  * FROM iot1
+        ORDER by date DESC
+        LIMIT {count}
+    """
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    return rows
 
-def select_all_tasks(conn: Connection, count:
+@app.get("/")
+def read_root():
+    return {"Hello": "robert"}
+
+@app.get("/items/{item_id}")
+async def read_item1(item_id:int):
+    return {"item_id": item_id}
+
+#query parameter
+@app.get("/raspberry")
+async def read_item(time:str = datetime.now().strftime("%Y%m%d %H:%M:%S"),light: float = 0.0, temperature: float = 0.0):
+    conn = create_connection('data.db')
+    if conn is not None:
+        create_table(conn)
+        insert_project(conn, (time,light,temperature))
+        conn.close()
+
+    return {
+        "時間":time,
+        "光線":light,
+        "溫度":temperature
+    }
+
+#query parameter
+@app.get("/iot_json/{item_count}")
+async def read_item2(item_count:int):
+    conn = create_connection('data.db')
+    if conn is not None:
+        create_table(conn)
+        rows = select_all_tasks(conn, item_count)            
+        conn.close()
+        return rows
